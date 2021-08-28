c3f572c348ccb62d219c2d29356fee5208818158
George Reilly <george@reilly.org>
2021-08-28 10:34:57 -0700

    Screen coordinates

--- diagrams.py
+++ diagrams.py
@@ -11,33 +11,30 @@ import os
 import sys
 
 SCRIPT_DIR = os.path.normpath(os.path.dirname(os.path.dirname(__file__)))
-print(SCRIPT_DIR)
 sys.path.insert(0, SCRIPT_DIR)
 
-from flags.usa import star_polygon, star_inner_radius, star_outer_radius
+from flags.usa import star_polygon, STAR_INNER_RADIUS_RATIO
 
 
 COLORS = ["Red", "Green", "Blue", "Black", "YellowGreen", "Cyan", "Magenta",
           "Orange", "Gray", "Maroon", "Indigo", "Teal",]
 
-def diagram(
+def svg_diagram(
         image_width: int = 500,
-        multiplier: int = 2,
         line_width: int = 0,
         filename: str = "star.svg",
     ):
-    image_width *= multiplier
-    image_height = image_width
-    CX = CY = int(image_width / 2)
-    outer_radius = CX * 0.95
-    inner_radius = star_inner_radius(outer_radius * 2)
-    ratio = inner_radius / outer_radius
+
+    image_height = image_width * 1.1
+    CX, CY = image_width // 2, image_height // 2
+    X_OFS, Y_OFS = 0, (image_height - image_width) // 2
+    outer_radius = CX
+    inner_radius = outer_radius * STAR_INNER_RADIUS_RATIO
 
     def pt(x, y, r=outer_radius, cx=0.0, cy=0.0):
-        assert -0.5 <= x - cx <= +0.5, str(x)
-        assert -0.5 <= y - cy <= +0.5, str(y)
-        px = round(CX + r * x)
-        py = round(CY - r * y) # subtract because image origin is at top-left
+        assert 0.0 <= x <= 1.0, str(x)
+        assert 0.0 <= y <= 1.0, str(y)
+        px, py = X_OFS + round(2 * r * x), Y_OFS + round(2 * r * y)
         return (px, py)
 
     dwg = sw.Drawing(filename=filename, size=(image_width, image_height), debug=True)
@@ -51,13 +48,13 @@ def diagram(
     dwg.add(dwg.polygon(inner_pentagon).fill("none").stroke("blue").dasharray([3, 3]))
     dwg.add(dwg.polygon(outer_pentagon).fill("none").stroke("gray").dasharray([3, 3]))
 
-    dwg.add(dwg.circle(center=(CX, CY), r=outer_radius/2).fill("none").stroke("green").dasharray([2, 3]))
-    dwg.add(dwg.circle(center=(CX, CY), r=inner_radius/2).fill("none").stroke("purple").dasharray([2, 3]))
+    dwg.add(dwg.circle(center=(CX, CY), r=outer_radius).fill("none").stroke("green").dasharray([2, 3]))
+    dwg.add(dwg.circle(center=(CX, CY), r=inner_radius).fill("none").stroke("purple").dasharray([2, 3]))
 
     # hx = where the y=0 line intercepts the right side of the star
-    hx = (ratio / math.tan(math.radians(36))) * outer_radius / 2
+    hx = (STAR_INNER_RADIUS_RATIO / math.tan(math.radians(36))) * outer_radius
     x1, x2 = -hx + CX, hx + CX
-    y1, y2 = CY + inner_radius / 2, CY - outer_radius / 2
+    y1, y2 = CY + inner_radius, CY - outer_radius
     dwg.add(dwg.line((x1, CY), (x2, CY)).stroke("cornflowerblue", width=2).dasharray([4, 4]))
     dwg.add(dwg.line((CX, y1), (CX, y2)).stroke("aqua", width=2).dasharray([4, 4]))
 
@@ -69,4 +66,4 @@ def diagram(
 
 
 if __name__ == "__main__":
-    diagram()
+    svg_diagram()
--- usa.py
+++ usa.py
@@ -9,34 +9,28 @@ from PIL import Image, ImageDraw  # type: ignore
 import math
 import os
 
-def star_outer_radius(diameter: float = 1.0) -> float:
-    return (diameter / 2)
+STAR_INNER_RADIUS_RATIO = math.sin(math.radians(18)) / math.sin(math.radians(54))
 
 
-def star_inner_radius(diameter: float = 1.0) -> float:
-    return star_outer_radius(diameter) * math.sin(math.radians(18)) / math.sin(math.radians(54))
-
-
-def star_polygon(diameter: float = 1.0, cx: float = 0.0, cy: float = 0.0) -> List[Tuple[float, float]]:
+def star_polygon() -> List[Tuple[float, float]]:
     start_angle = 90
-    outer_radius = star_outer_radius(diameter)
-    inner_radius = star_inner_radius(diameter)
+    outer_radius = 1.0 / 2.0
+    inner_radius = STAR_INNER_RADIUS_RATIO * outer_radius
     points = []
 
     for i in range(10):
         radius = inner_radius if i & 1 else outer_radius
         angle = (36 * i + start_angle) % 360
         rad = math.radians(angle)
-        x, y = (cx + radius * math.cos(rad), cy + radius * math.sin(rad))
-        points.append((x, y))
+        x, y = (radius * math.cos(rad), radius * math.sin(rad))
+        sx, sy = 0.5 + x, 0.5 - y
+        points.append((sx, sy))
 
     return points
 
 
 def draw_star(
         diameter: float = 1.0,
-        cx: float = 0.0,
-        cy: float = 0.0,
         fill_polygon: bool = True,
         image_width: int = 500,
         multiplier: int = 2,
@@ -48,13 +42,12 @@ def draw_star(
     image_width *= multiplier
     image_height = image_width
     CX = CY = int(image_width / 2)
-    star_radius = 2.0 * CX * 0.95
+    star_radius = image_width
 
     def pt(x, y):
-        assert -0.5 <= x - cx <= +0.5
-        assert -0.5 <= y - cy <= +0.5
-        px = round(CX + star_radius * x)
-        py = round(CY - star_radius * y) # subtract because image origin is at top-left
+        assert 0.0 <= x <= 1.0, str(x)
+        assert 0.0 <= y <= 1.0, str(y)
+        px, py = round(star_radius * x), round(star_radius * y)
         return (px, py)
 
     im = Image.new("RGB", (image_width, image_height), color=background_color)
@@ -63,27 +56,32 @@ def draw_star(
     colors = ["Red", "Green", "Blue", "Black", "YellowGreen", "Cyan", "Magenta",
               "Orange", "Gray", "Maroon", "Indigo", "Teal",]
 
-    outer_radius = star_outer_radius(diameter)
-    inner_radius = star_inner_radius(diameter)
-    points = star_polygon(diameter, cx, cy)
+    outer_radius = 1.0 / 2.0
+    inner_radius = STAR_INNER_RADIUS_RATIO * outer_radius
+    points = star_polygon()
 
     if fill_polygon:
         polygon = [pt(x, y) for x,y in points]
         draw.polygon(polygon, outline="Red", fill="White")
-        print(polygon)
+        # print(polygon)
     else:
         for i, (x, y) in enumerate(points):
             sx, sy = pt(x, y)
             p = i - 1 if i > 0 else len(points) - 1
             px, py = pt(*points[p])
-            print(f"i={i:2}, x={x:6.3f}, y={y:6.3f}, sx={sx:3}, sy={sy:3}, c={colors[i]}")
+            # print(f"i={i:2}, x={x:6.3f}, y={y:6.3f}, sx={sx:3}, sy={sy:3}, c={colors[i]}")
             draw.line([px, py, sx, sy], width=line_width, fill=colors[i])
 
     if quarters:
         # hx = where the y=0 line intercepts the right side of the star
         hx = inner_radius / math.tan(math.radians(36))
-        draw.line([*pt(-hx, 0), *pt(hx, 0)], width=line_width, fill="CornflowerBlue")
-        draw.line([*pt(0, -inner_radius), *pt(0, outer_radius)], width=line_width, fill="SeaGreen")
+        cx = cy = 0.5 # center is translated to (0.5, 0.5)
+        horiz = [*pt(cx - hx, cy), *pt(cx + hx, cy)]
+        vert = [*pt(cx, cy + inner_radius), *pt(cx, cy - outer_radius)]
+        draw.line(horiz, width=line_width, fill="CornflowerBlue")
+        draw.line(vert, width=line_width, fill="SeaGreen")
+
+    draw.ellipse((0, 0, image_width, image_height), fill=None, outline="Teal")
 
     # Scale down to smooth image
     if multiplier > 1:
@@ -94,4 +92,4 @@ def draw_star(
 
 
 if __name__ == "__main__":
-    draw_star(fill_polygon=True, line_width=3)
+    draw_star(fill_polygon=False, line_width=3)
